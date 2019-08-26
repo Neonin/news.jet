590aaa45e693e6f2ffc656de1d26e43b10255d1a
Mr.Neonin <mr.neonin@gmail.com>
2019-08-26 20:27:57 +0300

    Chore(Weather): added mutations, getters

--- src/store/weather.js
+++ src/store/weather.js
@@ -2,24 +2,37 @@ import axios from 'axios'
 
 const options = {
   baseURL: 'https://api.openweathermap.org/data/2.5/',
-  token: 'b156dce9f12975c304d7715cc08cf4ed'
+  token: 'b156dce9f12975c304d7715cc08cf4ed',
+  lang: 'ua'
 }
 
 export default {
   namespaced: true,
-  state: {},
-  mutations: {},
+  state: {
+    weather: []
+  },
+  mutations: {
+    setWeather (state, weather) {
+      const candidate = state.weather.find(c => c.id === weather.id)
+
+      if (!candidate) {
+        state.weather.push(weather)
+      }
+    }
+  },
   actions: {
     async fetchWeather ({ commit }, city) {
       try {
-        const response = await axios.get(`${options.baseURL}/weather?q=${city}&appid=${options.token}`)
+        const response = await axios.get(`${options.baseURL}/weather?q=${city}&units=metric&lang=${options.lang}&appid=${options.token}`)
         const data = response.data
 
-        console.log(data)
+        commit('setWeather', data)
       } catch (error) {
         console.log(error)
       }
     }
   },
-  getters: {}
+  getters: {
+    getWeather: state => state.weather
+  }
 }
